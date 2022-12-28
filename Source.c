9d83c725e7aa8aadd7d88b3f5da563d55b146935
DuckFromLake <121292875+DuckFromLake@users.noreply.github.com>
2022-12-28 05:04:07 +0300

    Add files via upload

--- Source.c
+++ Source.c
@@ -3,64 +3,57 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define TEAM 5
 #define CATAGORY 5
+#define TEAM 9
 
+/////////////////// FUNCTION LIST //////////////////////
 
+void read_file(FILE* file, int* L);
 void var_declaration_and_sort(int** Var, int** A, int* L, int Team);
 
-void arr(int** A,int* L, int Team);
+void arr(int** A, int* L, int Team);
 void rate(int** Var, int Team);
 
 int search_max(int** Var, int Team);
 int search_min(int** Var, int Team);
 
-int change_score_y(int y);
-int change_score_x(int x);
+void change_game(int** Var, int** A, int* L, int Team);
 
 
+/////////////////// MAIN //////////////////////
 
 
 void main()
 {
-	int x = 0, y = 0;
-	int max = 0, min = 0;
-	int i = 0;
-
-	// Score 0
-	// Place 1
-	// Wins 2
-	// Non 3
-	// Team 4
-
-	int **Var;
+	int a = 0, b, c, ta, j;
+	int max = 0, min = 0, tmax = 0, tmin = 0;
+	float tav = 0;
+	int** Var;
 	int** A;
-	int L[50];
+	int* L;
+	FILE* file;
+	file = fopen("Kurs.txt", "r");
 
-	Var = (double**)malloc(TEAM * sizeof(double*));
-	for (int i = 0; i < TEAM; i++)
-		Var[i] = (double*)malloc(CATAGORY * sizeof(double));
+	Var = (double**)malloc(CATAGORY * sizeof(double*));
+	for (int i = 0; i < CATAGORY; i++)
+		Var[i] = (double*)malloc(TEAM * sizeof(double));
 
 	A = (double**)malloc(TEAM * sizeof(double*));
 	for (int i = 0; i < TEAM; i++)
 		A[i] = (double*)malloc(TEAM * sizeof(double));
 
-	FILE* file;
-	file = fopen("Kurs.txt", "r");
-	while (fscanf(file, "%1i", &L[i]) != EOF) {
-		i++;
-	}
+	L = (double*)malloc(((((TEAM * TEAM) - TEAM) + 1) / 2) * sizeof(double));
 
+	read_file(file, L);
 	var_declaration_and_sort(Var, A, L, TEAM);
 
-	int a, b;
-	while (1) {
-		printf("Кёрлинг\n\nВыберете функцию:\n1)Таблица счёта\n2)Рейтинговая таблица\n3)Поиск команды\n4)Изменение ячейки\n5)Выход");
+	while (a != 5) {
+		printf("\n\nВыберете функцию:\n1)Таблица счёта\n2)Рейтинговая таблица\n3)Поиск команды\n4)Изменение ячейки\n5)Данные по команде\n5)Выход\n");
 		scanf("%d", &a);
 		switch (a) {
 		case 1:
 			system("cls");
-			arr(A,L,TEAM);
+			arr(A, L, TEAM);
 			printf("\n\n");
 			break;
 		case 2:
@@ -71,17 +64,17 @@ void main()
 		case 3:
 			system("cls");
 
-			printf("1)Поиск лучшей команды\n2)Поиск худшей команды\n");
+			printf("1)Поиск лучшей команды по очкам\n2)Поиск худшей команды по очкам\n");
 			scanf("%i", &b);
 			switch (b) {
 			case 1:
 				system("cls");
-				max = search_max(Var,TEAM);
+				max = search_max(Var, TEAM);
 				printf("Лучший счёт:\nКоманда %i (Счёт: %i)\n\n", Var[4][max], Var[0][max]);
 				break;
 			case 2:
 				system("cls");
-				min = search_min(Var,TEAM);
+				min = search_min(Var, TEAM);
 				printf("Худший счёт:\nКоманда %i (Счёт: %i)\n\n", Var[4][min], Var[0][min]);
 				break;
 			default:
@@ -91,34 +84,28 @@ void main()
 			break;
 		case 4:
 			system("cls");
-			printf("Ось x ячейки:");
-			scanf("%i", &x);
-			x--;
-			printf("Ось y ячейки:");
-			scanf("%i", &y);
-			y--;
-			printf("Новое значение ячейки Команда %i VS Команда %i\n", y + 1, x + 1);
-			if (y < x) {
-				L[A[y][x] * 2] = change_score_y(y);
-				L[((A[y][x] + 1) * 2) - 1] = change_score_x(x);
-			}
-			else if (y > x) {
-				L[((A[x][y] + 1) * 2) - 1] = change_score_y(y);
-				L[A[x][y] * 2] = change_score_x(x);
-			}
-
-			for (int i = 0; i < CATAGORY; i++) {
-				for (int j = 0; j < TEAM; j++) {
-					Var[i][j] = 0;
-				}
-			}
-
+			change_game(Var, A, L, TEAM);
 			var_declaration_and_sort(Var, A, L, TEAM);
+			break;
+		case 5:
+			fclose(file);
+			printf("\nВыход из программы\n");
 			break;
 		default:
 			printf("Введён некорректный номер функции!\n\n");
 			break;
 		}
+	}
+}
+
+
+/////////////////// FUNCTIONS //////////////////////
+
+
+void read_file(FILE* file, int* L) {
+	int i = 0;
+	while (fscanf(file, "%1i", &L[i]) != EOF) {
+		i++;
 	}
 }
 
@@ -166,7 +153,7 @@ void var_declaration_and_sort(int** Var, int** A, int* L, int Team) {
 	for (int ch = 1; ch == 1; ch = ch) {
 		ch = 0;
 		for (int i = 1;i < Team; i++) {
-			if ((Var[2][i] > Var[2][i - 1]) || ((Var[2][i] == Var[2][i - 1]) && (Var[0][i] > Var[0][i - 1]))) {
+			if ((Var[2][i] > Var[2][i - 1]) || ((Var[2][i] == Var[2][i - 1]) && (Var[0][i] > Var[0][i - 1])) || ((Var[2][i] == Var[2][i - 1]) && (Var[0][i] == Var[0][i - 1])) && (Var[3][i] > Var[3][i - 1])) {
 				ch = 1;
 				x = Var[2][i];
 				Var[2][i] = Var[2][i - 1];
@@ -185,7 +172,7 @@ void var_declaration_and_sort(int** Var, int** A, int* L, int Team) {
 	}
 	Var[1][0] = 1;
 	for (int i = 1; i < Team; i++) {
-		if (Var[0][i - 1] == Var[0][i]) {
+		if (Var[0][i - 1] == Var[0][i] && Var[3][i - 1] == Var[3][i]) {
 			Var[1][i] = Var[1][i - 1];
 		}
 		else {
@@ -227,7 +214,7 @@ void rate(int** Var, int Team) {
 
 	printf("  Место  |   Команда  | Победы  |  Ничья  | Поражения | Набранные очки |\n");
 	for (int i = 0; i < Team; i++) {
-		printf("  %3i    |  Команда %i |  %3i    |  %3i    |  %4i     |      %3i       |\n", Var[1][i], Var[4][i], Var[2][i], Var[3][i], TEAM - 1 - Var[2][i] - Var[3][i], Var[0][i]);
+		printf("  %3i    |  Команда %i |  %3i    |  %3i    |  %4i     |      %3i       |\n", Var[1][i], Var[4][i], Var[2][i], Var[3][i], Team - 1 - Var[2][i] - Var[3][i], Var[0][i]);
 	}
 }
 
@@ -256,16 +243,40 @@ int search_min(int** Var, int Team) {
 	}
 	return (min);
 }
-int change_score_y(int y) {
-	int n;
-	printf("Очки команды %i:", y + 1);
+
+void change_game(int** Var, int** A, int* L, int Team) {
+	int x = 0, y = 0;
+	int m, n;
+
+	system("cls");
+	printf("Команда по горизонтали:");
+	scanf("%i", &x);
+	printf("Команда по вертикали:");
+	scanf("%i", &y);
+	printf("Новое значение ячейки Команда %i VS Команда %i\n", y, x);
+
+	printf("Очки команды %i:", x);
 	scanf("%i", &n);
-	return n;
+
+	printf("Очки команды %i:", y);
+	scanf("%i", &m);
+
+	y--;
+	x--;
+
+	if (y < x) {
+		L[A[y][x] * 2] = m;
+		L[((A[y][x] + 1) * 2) - 1] = n;
+	}
+	else if (y > x) {
+		L[((A[x][y] + 1) * 2) - 1] = m;
+		L[A[x][y] * 2] = n;
+	}
+
+	for (int i = 0; i < CATAGORY; i++) {
+		for (int j = 0; j < Team; j++) {
+			Var[i][j] = 0;
+		}
+	}
 }
 
-int change_score_x(int x) {
-	int n;
-	printf("Очки команды %i:", x + 1);
-	scanf("%i", &n);
-	return n;
-}
